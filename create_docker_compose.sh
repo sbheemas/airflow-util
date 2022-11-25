e9de7fccc02b479e7d3bf29587ed3e1450bf1f8d
Akash Jena <97431363+akash-jena@users.noreply.github.com>
2022-11-25 10:44:30 +0000

    added symlink for sudo access to docker-compose

--- create_docker_compose.sh
+++ create_docker_compose.sh
@@ -3,5 +3,6 @@
 echo "Installing docker-compose..."
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose version
 echo "Dcoker compose has been successfully installed"
